4dc173ee806f9d85dcaaa2264678dad1598fa6f1
jonathan_willis <jwillis@publipage.com>
2015-09-03 17:34:27 -0400

    Added another iplocation service.

--- example/js/main.js
+++ example/js/main.js
@@ -57,6 +57,7 @@ var LocationServices = {
       ipapi:        createApiCall('http://ip-api.com/json/')
     , freegeoip:    createApiCall('http://freegeoip.net/json/')
     , ipinfo:       createApiCall('http://ipinfo.io/')
+    , telize:       createApiCall('http://www.telize.com/geoip')
 };
 
 var createFakeApiCall = jaw.promises.createFakeApiCall;
@@ -64,6 +65,7 @@ var LocationServices = {
       ipapi:        createFakeApiCall({"url":"http://ip-api.com/json/", "as":"AS5645 TekSavvy Solutions, Inc.","city":"Toronto","country":"Canada","countryCode":"CA","isp":"TekSavvy Solutions","lat":43.6899,"lon":-79.3853,"org":"TekSavvy Solutions","query":"107.179.159.142","region":"ON","regionName":"Ontario","status":"success","timezone":"America/Toronto","zip":"M4T"}, undefined, 300)
     , freegeoip:    createFakeApiCall({"url":"http://freegeoip.net/json/", "ip":"107.179.159.142","country_code":"CA","country_name":"Canada","region_code":"","region_name":"","city":"","zip_code":"","time_zone":"","latitude":43.643,"longitude":-79.388,"metro_code":0},undefined, 300)
     , ipinfo:       createFakeApiCall({"url":"http://ipinfo.io/", "ip": "107.179.159.142","hostname": "107-179-159-142.cpe.teksavvy.com","city": null,"country": "CA","loc": "43.6425,-79.3872","org": "AS5645 TekSavvy Solutions, Inc."},undefined, 300)
+    , telize:       createFakeApiCall({"url":"http://www.telize.com/geoip", "country_code3":"CAN","country":"Canada","isp":"TekSavvy Solutions, Inc.","country_code":"CA","dma_code":"0","continent_code":"NA","area_code":"0","ip":"107.179.159.142","asn":"AS5645","latitude":43.6425,"longitude":-79.3872})
 };
 
 var manipulators = jaw.promises.manipulators;
@@ -71,6 +73,7 @@ var LocationServicesNormalized = {
       ipapi: manipulators.pluck(LocationServices.ipapi, ['url', 'lat', 'lon'])
     , freegeoip: manipulators.combine(LocationServices.freegeoip, [ jaw.data.pluck, ['url', 'latitude', 'longitude'] ], [ jaw.data.remap, { latitude: 'lat', longitude: 'lon'} ])
     , ipinfo: manipulators.combine(LocationServices.ipinfo, [ jaw.data.pluck, ['url', 'loc'] ], [ jaw.data.reduce, collectData, {}])
+    , telize: manipulators.combine(LocationServices.telize, [ jaw.data.pluck, ['url', 'latitude', 'longitude'] ], [ jaw.data.remap, { latitude: 'lat', longitude: 'lon'} ])
 };
 
 
@@ -104,16 +107,17 @@ function getHostnameFromUrl(url) {
     return a.hostname==='' ? url : a.hostname;
 }
 
-/**
- *
- * @param location {object}     lat, lon, name
- */
 var itemRowRenderer = createRendererWithId('item-row-tpl');
+
+/**
+ * @param location {object}     lat, lon, url
+ */
 function renderLocation(mapSymbol, location, map) {
     var point = new mxn.LatLonPoint(location.lat, location.lon);
     var marker = new mxn.Marker(point);
     var iconUrl = generateIconImageUrl(mapSymbol.toString());
     marker.setIcon(iconUrl, [21, 34]);
+    marker.setShadowIcon('http://chart.apis.google.com/chart?chst=d_map_pin_shadow');
     marker.setLabel(location.url);
     marker.setInfoBubble(location.url);
     map.addMarker(marker);
@@ -126,34 +130,13 @@ function renderLocation(mapSymbol, location, map) {
     $('.service-feedback').append(html);
 }
 
-function renderEachLocation(locations, map, delay) {
-    if( typeof delay!=='number' ) delay = 1000;
-
-    return new Promise(function(resolve, reject) {
-        function helper(i, locations) {
-            var location = locations.shift();
-
-            if ( location===undefined ) {
-                resolve();
-                return;
-            }
-            setTimeout(function() {
-                renderLocation(i, location, map);
-                helper(i+1, locations);
-            }, delay);
-        }
-        helper(1, locations);
-    });
-}
-
 function main() {
 
     locationLookups = _.values(LocationServicesNormalized);
-    map = createMap('map', 'openlayers');
-    //map = createMap('map', 'googlev3');
+    //map = createMap('map', 'openlayers');
+    map = createMap('map', 'googlev3');
 
     jaw.promises.reduce(locationLookups, function(acc, location) {
-        // abuse of reduce for side effects
         console.log('step', location);
 
         acc.push(location);
@@ -165,12 +148,6 @@ function main() {
             lon: avg.apply(this, _.pluck(locations, 'lon'))
         };
         console.log('Avg', avgLocation);
-
-        //renderEachLocation(locations, map)
-        //    .then(function() {
-        //        renderLocation('*', avgLocation, map);
-        //        map.autoCenterAndZoom();
-        //    });
 
         locations.forEach(function(location, i) {
             renderLocation(i+1, location, map);
